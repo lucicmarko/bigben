95efb87ed9a11503d36d55328a222ea337f8bf0e
Marko Lucic <Lucic@fidor.com>
2017-12-24 19:53:15 +0100

    rescue from not found

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,15 @@
 class ApplicationController < ActionController::API
   before_action :authenticate_request
+
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found
+
   attr_reader :current_user
 
   private
+
+  def not_found
+    render status: :not_found, json: ''
+  end
 
   def authenticate_request
     @current_user = AuthorizeApiRequest.call(request.headers).result 
